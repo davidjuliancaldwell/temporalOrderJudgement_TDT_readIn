b697281ccbe08a12e7ef9ff92c1096f8c5a26c18
David Caldwell <davidjuliancaldwell@gmail.com>
2018-04-23 19:38:36 -0700

    generate read in file in MATLAB, before moving to Matlab interface for saying yes no with rating scale

--- UserFiles/generateTOJ_concatenated.m
+++ UserFiles/generateTOJ_concatenated.m
@@ -1,16 +1,15 @@
 %% DJC - 4-12-2018
 % script to generate TOJs to be read in
-
+timeToPerception = input('What was the rough time to perception? (ms) \n');
 delayRange = input('What is the range of delays to use? (in milliseconds, with 0 being tactor delivery. \n -150 would be 150 ms before tactor, +150 would be 150 ms after tactor \n input as [-150,150] \n');
 distBetween = input('What is the distance between each offset (ms) we want to use? \n '); % 10 means 10 ms increments, 1 means 1 ms increments, 50 would be 50 ms increments
 delayRange = [delayRange(1):distBetween:delayRange(2)];
-
 fprintf(['This is at least ' num2str(length(delayRange)) ' trials to cover each element in the vector \n \n']);
 %
 numTrials = input('What is the number of repetitions of the above vector within each block? \n');
 numBlocks = input('How many blocks do we want to run? \n');
-
 delaysTotal = [];
+delayRange = delayRange - timeToPerception;  
 
 %%
 for i = 1:numBlocks
